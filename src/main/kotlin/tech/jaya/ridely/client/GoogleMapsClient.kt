9b7582cb5d52eae99b7354e868169ac329077e38
joana santos <souzajoana18@gmail.com>
2025-07-07 23:45:09 -0300

    feat: add GoogleMapsClient for route estimation

--- src/main/kotlin/tech/jaya/ridely/client/GoogleMapsClient.kt
+++ src/main/kotlin/tech/jaya/ridely/client/GoogleMapsClient.kt
@@ -1,4 +1,15 @@
 package tech.jaya.ridely.client
 
+import org.springframework.stereotype.Component
+import tech.jaya.ridely.dto.RouteInfo
+
+@Component
 class GoogleMapsClient {
+    fun getRouteInfo(pickUp: String, dropOff: String): RouteInfo {
+        return RouteInfo(
+            estimatedTimeMinute = 15, // Simulated value
+            distanceKm = 10.0 // Simulated value
+        )
+
+    }
 }
