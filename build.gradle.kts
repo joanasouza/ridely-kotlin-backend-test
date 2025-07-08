a9b001ff3f4f2e2f6b6638dcaf35b2c33d5ffea3
joana santos <souzajoana18@gmail.com>
2025-07-08 00:07:53 -0300

    test: configure test profile and add test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
 	runtimeOnly("org.flywaydb:flyway-mysql:$flywayVersion")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.10")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<KotlinCompile> {
